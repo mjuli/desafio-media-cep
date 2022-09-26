458e5b43314ffd5dec8ae4dc7290eabff0cbf22d
Julia Martins <mjuh.ms@gmail.com>
2022-09-26 20:56:27 -0300

    Changing imports

--- test/media.test.js
+++ test/media.test.js
@@ -1,5 +1,5 @@
-import request from 'supertest'
-import app from '../server'
+const request = require('supertest')
+const app = require('../server')
 
 describe('GET /media', () => {
   const URL = '/media'
